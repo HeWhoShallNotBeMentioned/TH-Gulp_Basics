7cb36e02239d0dbd47edbbf12a306c59561d756c
Chris Underwood <cunderwoodmn@gmail.com>
2018-02-02 20:10:20 -0800

    add build which will run all of our building tasks and put that in default

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const sass = require('gulp-sass');
 const maps = require('gulp-sourcemaps');
 
 gulp.task('concatScripts', function () {
-  gulp.src([
+  return gulp.src([
     "js/jquery.js",
     "js/sticky/jquery.sticky.js",
     "js/main.js"])
@@ -18,21 +18,21 @@ gulp.task('concatScripts', function () {
   .pipe(gulp.dest("js"));
 });
 
-gulp.task('minifyScripts', function (){
-  gulp.src("js/app.js")
+gulp.task('minifyScripts',['concatScripts'] , function (){
+  return gulp.src("js/app.js")
   .pipe(uglify())
   .pipe(rename('app.min.js'))
   .pipe(gulp.dest('js'));
 });
 
 gulp.task("compileSass", function(){
-  gulp.src("scss/application.scss")
+  return gulp.src("scss/application.scss")
   .pipe(maps.init())
   .pipe(sass())
   .pipe(maps.write('./'))
   .pipe(gulp.dest('css'));
 });
 
-gulp.task('default', ['hello'], function () {
-  console.log("The default task!");
-});
+gulp.task('build', ['minifyScripts', 'compileSass'])
+
+gulp.task('default', ['build']);
