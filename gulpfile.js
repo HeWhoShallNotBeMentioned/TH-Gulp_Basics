8622ac70dda50203d942171dae6bc8c605daf797
Chris Underwood <cunderwoodmn@gmail.com>
2018-02-04 21:32:10 -0800

    add gulp-iff and gulp-csso along with edits to the gulpfile.js file to get the html task to run in the build task

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ const maps    = require('gulp-sourcemaps');
 const del     = require('del');
 const connect = require('gulp-connect');
 const useref  = require('gulp-useref');
+const iff     = require('gulp-if');
+const csso    = require('gulp-csso');
 var options = {
   src: 'src',
   dist: 'dist'
@@ -18,6 +20,14 @@ var options = {
 //   .pipe(uglify())
 //   .pipe(rename('app.min.js'))
 //   .pipe(gulp.dest('js'));
+// });
+
+// gulp.task("compileSass", function(){
+//   return gulp.src("scss/application.scss")
+//   .pipe(maps.init())
+//   .pipe(sass())
+//   .pipe(maps.write('./'))
+//   .pipe(gulp.dest('css'));
 // });
 
 gulp.task("compileSass", function(){
@@ -36,11 +46,17 @@ gulp.task('clean', function (){
   del(['dist', 'css/application.css*', 'js/app*.js*']);
 });
 
+gulp.task('html', function () {
+    return gulp.src('src/index.html')
+        .pipe(useref())
+        .pipe(iff('*.js', uglify()))
+        .pipe(iff('*.css', csso()))
+        .pipe(gulp.dest(options.dist));
+});
 
-
-gulp.task('build', ['minifyScripts', 'compileSass'], function () {
-  return gulp.src(['css/application.css', 'js/app.min.js', 'index.html', 'img/**', 'fonts/**'], {base: './'})
-    .pipe(gulp.dest('dist'));
+gulp.task('build', ['html'], function () {
+  return gulp.src([options.src + '/img/**', options.src +'/fonts/**'], {base: options.src})
+    .pipe(gulp.dest(options.dist));
 });
 
 gulp.task('serve', ['watchFiles'], function () {
